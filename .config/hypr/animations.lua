e59b610b1cf76c4f9099772e0c35fea8eeaa9595
chihingordonshi <chihin.gordon.shi@gmail.com>
2026-09-13 14:03:07 -0400

    window out animation

--- .config/hypr/animations.lua
+++ .config/hypr/animations.lua
@@ -8,15 +8,17 @@ hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}
 hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
 hl.curve("swift",          { type = 'bezier', points = { {0.55, 0},    {0.1, 1}     } })
 hl.curve("springIn",       { type = "bezier", points = { {0.66, 0.13}, {0.27, 1.61} } })
+hl.curve("emphasizeDecel", { type = "bezier", points = { {0.05, 0.70}, {0.10, 1.00} } })
 
 -- Default springs
 hl.curve("easy",           { type = "spring", mass = 1, stiffness = 900, dampening = 60 })
 hl.curve("poppin",         { type = "spring", mass = 1, stiffness = 200, dampening = 25 })
-hl.curve("wiggle",         { type = "spring", mass = 1, stiffness = 100, dampening = 10})
+hl.curve("wiggle",         { type = "spring", mass = 1, stiffness = 100, dampening = 10 })
 
-hl.animation({ leaf = "global",        enabled = true,  speed = 3,    bezier = "default" })
+hl.animation({ leaf = "global",        enabled = true,  speed = 3,    bezier = "default"      })
 hl.animation({ leaf = "border",        enabled = true,  speed = 3,    bezier = "easeOutQuint" })
-hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
-hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
-hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1.94, bezier = "almostLinear",  style = "fade" })
-hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick" })
+hl.animation({ leaf = "fade",          enabled = true,  speed = 3,    bezier = "quick"        })
+hl.animation({ leaf = "layers",        enabled = true,  speed = 4,    bezier = "easeOutQuint" })
+hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1,    bezier = "almostLinear",   style = "fade" })
+hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick"        })
+hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 7,    bezier = "emphasizeDecel", style = "popin 60%" })
